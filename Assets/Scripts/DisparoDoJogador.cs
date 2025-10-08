fa3880b75994a661ad69167097be3ed62146f486
Gabrielly <gabrielly.milena.mr@gmail.com>
2025-10-07 21:11:48 -0300

    Arrumado

--- Assets/Scripts/DisparoDoJogador.cs
+++ Assets/Scripts/DisparoDoJogador.cs
@@ -3,28 +3,13 @@ using UnityEngine;
 public class DisparoDoJogador : MonoBehaviour
 {
     public GameObject prefabLaser;
-    private Color corAtual = Color.white;
+    [HideInInspector]
+    public Color corAtual = Color.white;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.JoystickButton0)) {
-            corAtual = Color.green;
-            Debug.Log("Cor atual: Verde");
-        }
-        else if (Input.GetKeyDown(KeyCode.JoystickButton1)) {
-            corAtual = Color.blue;
-            Debug.Log("Cor atual: Azul");
-        }
-        else if (Input.GetKeyDown(KeyCode.JoystickButton2)) {
-            corAtual = Color.red;
-            Debug.Log("Cor atual: Vermelho");
-        }
-        else if (Input.GetKeyDown(KeyCode.JoystickButton3)) {
-            corAtual = Color.yellow;
-            Debug.Log("Cor atual: Amarelo");
-        }
-
-        if (Input.GetKeyDown(KeyCode.JoystickButton5) || Input.GetKeyDown(KeyCode.Space))
+        // Disparo com botão do controle ou espaço
+        if (Input.GetKeyDown(KeyCode.JoystickButton0) || Input.GetKeyDown(KeyCode.Space))
         {
             if (prefabLaser == null)
             {
@@ -36,10 +21,6 @@ public class DisparoDoJogador : MonoBehaviour
             var laserScript = laser.GetComponent<LaserDoJogador>();
             if (laserScript != null)
                 laserScript.DefinirCor(corAtual);
-            else
-                Debug.LogWarning("⚠️ O prefab não contém o script LaserDoJogador!");
         }
     }
 }
-
-
